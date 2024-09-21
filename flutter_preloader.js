8efe4eaf6c4c7bee7d553b60df8bf73d8b0c7ded
callumturnbull00 <callumturnbull00@gmail.com>
2024-09-21 09:19:27 +0100

    Update flutter_preloader.js

--- flutter_preloader.js
+++ flutter_preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://cdn.jsdelivr.net/gh/callumturnbull00/repository1/loading-svgrepo-com.svg';
+svgImage.src = 'https://cdn.jsdelivr.net/gh/callumturnbull00/repository1/bouncing-ball.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
